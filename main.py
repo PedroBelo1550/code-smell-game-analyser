851c35618cbc420c16478c6035896dd4069ffa7e
pedro.belo <pedrohenrique1550@hotmail.com>
2024-04-21 17:09:40 -0300

    compose

--- main.py
+++ main.py
@@ -1,31 +1,54 @@
+import pandas as pd
 import os
 import shutil
-
-from analyser import Analyser
 from sonar import Sonar
 
-id_jogo = '13º signo - 2º período - 2-2016'
-pasta_projeto = 'C:\\Users\\vm1\\Documents\\dev\\analise_sonar'
-origem = 'E:\\jogos'
+nome_arq = 'Catalogação.csv'
+df = pd.read_csv(nome_arq, delimiter=';')
 
-if os.path.exists(pasta_projeto):
-    shutil.rmtree(pasta_projeto)
-    os.makedirs(pasta_projeto)
-else:
-    os.makedirs(pasta_projeto)
+for index, row in df.iterrows():
+    print(row['pasta'])
+    try:
 
-print('copiando os dados')
-origem = os.path.join(origem, id_jogo)
-destino = os.path.join(pasta_projeto, id_jogo)
-#Copia os dados
-shutil.copytree(origem, destino)
+        id_jogo = row['pasta']
+        pasta_projeto = 'C:\\Users\\vm1\\Documents\\dev\\analise_sonar'
+        origem = 'E:\\jogos'
 
-#Analyser.processar(id_jogo, destino)
-Sonar.executa_scanne(id_jogo)
-Sonar.obter_métricas(id_jogo)
+        if os.path.exists(pasta_projeto):
+            shutil.rmtree(pasta_projeto)
+            os.makedirs(pasta_projeto)
+        else:
+            os.makedirs(pasta_projeto)
 
-print('deletando pastas')
-shutil.rmtree(destino)
-shutil.rmtree(pasta_projeto)
-shutil.rmtree('./jogo')
-os.makedirs(destino)
+        print('copiando os dados')
+        origem = os.path.join(origem, id_jogo)
+        destino = os.path.join(pasta_projeto, id_jogo)
+        # Copia os dados
+        shutil.copytree(origem, destino)
+
+        # Analyser.processar(id_jogo, destino)
+        Sonar.executa_scanne(id_jogo)
+        Sonar.obter_métricas(id_jogo)
+
+        print('deletando pastas')
+        shutil.rmtree(destino)
+        shutil.rmtree(pasta_projeto)
+        shutil.rmtree('./jogo')
+        os.makedirs(destino)
+
+        df.at[index, 'processado'] = True
+        df.to_csv(nome_arq, index=False)
+
+    except Exception as e:
+        shutil.rmtree(destino)
+        shutil.rmtree(pasta_projeto)
+        shutil.rmtree('./jogo')
+        os.makedirs(destino)
+        # Se ocorrer um erro, imprimir mensagem de erro e pular para a próxima iteração
+        print(f"Erro ao processar a linha {index}: {e}")
+        continue
+
+
+
+
+
--- teste.py
+++ teste.py
@@ -1,4 +1,54 @@
-from funcoes import Funcoes
+import pandas as pd
+import os
+import shutil
+from sonar import Sonar
+
+nome_arq = 'Catalogação.csv'
+df = pd.read_csv(nome_arq, delimiter=';')
+
+for index, row in df.iterrows():
+    print(row['pasta'])
+    try:
+
+        id_jogo = row['pasta']
+        pasta_projeto = 'C:\\Users\\vm1\\Documents\\dev\\analise_sonar'
+        origem = 'E:\\jogos'
+
+        if os.path.exists(pasta_projeto):
+            shutil.rmtree(pasta_projeto)
+            os.makedirs(pasta_projeto)
+        else:
+            os.makedirs(pasta_projeto)
+
+        print('copiando os dados')
+        origem = os.path.join(origem, id_jogo)
+        destino = os.path.join(pasta_projeto, id_jogo)
+        # Copia os dados
+        shutil.copytree(origem, destino)
+
+        # Analyser.processar(id_jogo, destino)
+        Sonar.executa_scanne(id_jogo)
+        Sonar.obter_métricas(id_jogo)
+
+        print('deletando pastas')
+        shutil.rmtree(destino)
+        shutil.rmtree(pasta_projeto)
+        shutil.rmtree('./jogo')
+        os.makedirs(destino)
+
+        df.at[index, 'processado'] = True
+        df.to_csv(nome_arq, index=False)
+
+    except Exception as e:
+        shutil.rmtree(destino)
+        shutil.rmtree(pasta_projeto)
+        shutil.rmtree('./jogo')
+        os.makedirs(destino)
+        # Se ocorrer um erro, imprimir mensagem de erro e pular para a próxima iteração
+        print(f"Erro ao processar a linha {index}: {e}")
+        continue
 
 
-print(Funcoes.json_para_csv('codeSmells.json'))
+
+
+
