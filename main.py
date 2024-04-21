a737c8b445321604bada454d1e8468f040dabdc8
Pedro belo <pedrohenrique1550@hotmail.com>
2024-04-21 20:57:26 +0000

    correcoes

--- main.py
+++ main.py
@@ -1,54 +1,42 @@
 import pandas as pd
 import os
 import shutil
+
+from analyser import Analyser
+from funcoes import Funcoes
 from sonar import Sonar
 
 nome_arq = 'Catalogação.csv'
-df = pd.read_csv(nome_arq, delimiter=';')
+df = pd.read_csv(nome_arq)
 
 for index, row in df.iterrows():
     print(row['pasta'])
+
     try:
+        if not row['processado']:
+            id_jogo = str(row['pasta'])
+            pasta_projeto = 'C:\\Users\\vm1\\Documents\\dev\\analise_sonar'
+            origem = 'E:\\jogos'
 
-        id_jogo = row['pasta']
-        pasta_projeto = 'C:\\Users\\vm1\\Documents\\dev\\analise_sonar'
-        origem = 'E:\\jogos'
-
-        if os.path.exists(pasta_projeto):
-            shutil.rmtree(pasta_projeto)
+            Funcoes.remove_folder(pasta_projeto)
+            Funcoes.remove_folder('./jogo')
+            os.makedirs('./jogo')
             os.makedirs(pasta_projeto)
-        else:
-            os.makedirs(pasta_projeto)
 
-        print('copiando os dados')
-        origem = os.path.join(origem, id_jogo)
-        destino = os.path.join(pasta_projeto, id_jogo)
-        # Copia os dados
-        shutil.copytree(origem, destino)
+            print('copiando os dados')
+            origem = os.path.join(origem, id_jogo)
+            destino = os.path.join(pasta_projeto, id_jogo)
+            # Copia os dados
+            shutil.copytree(origem, destino)
 
-        # Analyser.processar(id_jogo, destino)
-        Sonar.executa_scanne(id_jogo)
-        Sonar.obter_métricas(id_jogo)
-
-        print('deletando pastas')
-        shutil.rmtree(destino)
-        shutil.rmtree(pasta_projeto)
-        shutil.rmtree('./jogo')
-        os.makedirs(destino)
-
-        df.at[index, 'processado'] = True
-        df.to_csv(nome_arq, index=False)
+            Analyser.processar(id_jogo, destino)
+            Sonar.executa_scanne(id_jogo)
+            Sonar.obter_métricas(id_jogo)
 
     except Exception as e:
-        shutil.rmtree(destino)
-        shutil.rmtree(pasta_projeto)
-        shutil.rmtree('./jogo')
-        os.makedirs(destino)
         # Se ocorrer um erro, imprimir mensagem de erro e pular para a próxima iteração
         print(f"Erro ao processar a linha {index}: {e}")
         continue
 
-
-
-
-
+    df.at[index, 'processado'] = True
+    df.to_csv(nome_arq, index=False)
