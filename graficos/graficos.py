ac9f3fb4e6ef3fb1b12a5af31a741f76b3a78759
pedro.belo <pedrohenrique1550@hotmail.com>
2024-05-10 17:10:49 -0300

    d

--- graficos/graficos.py
+++ graficos/graficos.py
@@ -3,11 +3,11 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
 from scipy.stats import spearmanr
+from scipy.stats import kruskal
 
 df = pd.read_excel('graficos/data.xlsx', sheet_name='Export')
 
-dependente = 'Game Smells'
-
+dependente = 'periodo'
 independente = 'Code'
 
 # Calculando a mediana da complexidade por período
@@ -16,10 +16,24 @@ mediana_complexidade_por_periodo = df.groupby(independente)[dependente].median()
 # Calculando a regressão
 slope, intercept, r_value, p_value, std_err = linregress(df[independente], df[dependente])
 
-print("Equação de regressão: Complexity = {}*Período + {}".format(slope, intercept))
+print(f"Equação de regressão: {independente} = {slope} * {dependente} + {intercept}")
 print("Coeficiente de correlação de Pearson (r):", r_value)
 
-r, p_value = spearmanr(df[independente], df[dependente])
+# Coeficiente de correlação de Spearmanr
+r, p_value_spearman = spearmanr(df[independente], df[dependente])
 
 print("Coeficiente de correlação de Spearmanr (r):", r)
-print("Valor-p:", p_value)
+print("Valor-p Spearman:", p_value_spearman)
+
+# Teste de Kruskal-Wallis
+periodos = []
+for periodo, grupo in df.groupby(dependente):
+    periodos.append(grupo[independente])
+
+statistic, p_value_kruskal = kruskal(*periodos)
+
+alpha = 0.05
+if p_value_kruskal < alpha:
+    print(f"As medianas são estatisticamente diferentes (Rejeitamos a hipótese nula) - Resultado: {p_value_kruskal}")
+else:
+    print(f"As medianas não são estatisticamente diferentes (Falhamos em rejeitar a hipótese nula) - Resultado: {p_value_kruskal}")
